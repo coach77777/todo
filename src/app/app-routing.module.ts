e5b89422a6cf89985f3a6017105ce8633e301164
coach77777 <108881946+coach77777@users.noreply.github.com>
2023-07-23 13:01:49 -0500

    hardcoding ended

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RouteGuardService } from './service/route-guard.service';
 
 //welcome
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', component: LoginComponent },//canActivate, RouteGuardService
   { path: 'login', component: LoginComponent },
-  { path: 'welcome/:name', component: WelcomeComponent },
-  { path: 'todos', component: ListTodosComponent },
+  { path: 'welcome/:name', component: WelcomeComponent, canActivate: [RouteGuardService]  },
+  { path: 'todos', component: ListTodosComponent , canActivate: [RouteGuardService] },
+  { path: 'logout', component: LogoutComponent, canActivate: [RouteGuardService]  },
 
   { path: '**', component: ErrorComponent },
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
+import { HardcodedAuthenticationService } from './../service/hardcoded-authentication.service';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -15,13 +17,15 @@ export class LoginComponent {
   //Router
   //Angular.giveMeRouter
   //Dependency Injection
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+    private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
 
   ngOnInit() {}
 
   handleLogin() {
     // console.log(this.username);
-    if (this.username === 'craig' && this.password === 'puppy') {
+   // if (this.username === 'craig' && this.password === 'puppy') {
+      if (this.hardcodedAuthenticationService.authenticate(this.username,  this.password)) {
       //Redirect to welcome page
       this.router.navigate(['welcome', this.username])
       this.invalidLogin = false;
