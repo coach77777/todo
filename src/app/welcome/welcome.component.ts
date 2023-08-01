30dee4756fc97c015e90d81f25e668f5cc863f9c
coach77777 <108881946+coach77777@users.noreply.github.com>
2023-08-01 06:17:52 -0500

    Connecting Angular Frontend with Restful API

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,21 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css'],
 })
+
+//public class SpringBootFirstWebApp
 export class WelcomeComponent implements OnInit {
-  message = 'Some Message';
-  name = ' '
+  message = 'Some Message'
+  //  welcomeMessageFromService: string --failed to compile
+  welcomeMessageFromService: any
+  name = ' ';
 
   //ActivatedRoute
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private service: WelcomeDataService
+  ) {
+
+  }
+
+
   ngOnInit() {
-    console.log(this.message);
+    // console.log(this.message);
     // console.log (this.route.snapshot.params['name'])
 
- this.name = this.route.snapshot.params['name']
+    this.name = this.route.snapshot.params['name'];
   }
+  getWelcomeMessage() {
+    console.log(this.service.executeHelloWorldBeanService());
+
+    this.service.executeHelloWorldBeanService().subscribe(
+      //response =>  this.handleSuccessfulResponse(response)
+      (response) => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+      )
+
+
+    console.log('last line of getWelcomeMessage');
+
+    //console.log('get welcome');
+  }
+  handleSuccessfulResponse(response:any) {
+    this.welcomeMessageFromService = response.message;
+    //console.log(response);
+    //console.log(response.message);
+  }
+
+
+    handleErrorResponse(error: any) {
+      //console.log(error);
+      //console.log(error.error);
+      //console.log(error.error.message);
+      this.welcomeMessageFromService = error.error.message
+    }
 }
