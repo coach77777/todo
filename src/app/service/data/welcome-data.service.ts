5be4c86367d54e51833a4ac46955b95b334d0f17
coach77777 <108881946+coach77777@users.noreply.github.com>
2023-08-22 06:25:43 -0500

    87-Spring Security Configuring Basic Authorization

--- src/app/service/data/welcome-data.service.ts
+++ src/app/service/data/welcome-data.service.ts
@@ -1,29 +1,45 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-export class HelloWorldBean{
-
-  constructor (public message:string)
-{ }
-
+export class HelloWorldBean {
+  constructor(public message: string) {}
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class WelcomeDataService {
-
-  constructor(
-    private http: HttpClient
-    ) { }
+  constructor(private http: HttpClient) {}
 
   executeHelloWorldBeanService() {
-   return this.http.get<HelloWorldBean>('http://localhost:8080/hello-world-bean');
+    return this.http.get<HelloWorldBean>(
+      'http://localhost:8080/hello-world-bean'
+    );
     //console.log("Execute Hello World BEAN Service")
   }
 
   executeHelloWorldBeanServiceWithPathVariable(name: any) {
-    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`);
-     //console.log("Execute Hello World BEAN Service")
-   }
+    let basicAuthHeaderString = this.createBasicAuthenticationHttpHeader();
+
+    let headers = new HttpHeaders({
+      Authorization: basicAuthHeaderString,
+    });
+
+    return this.http.get<HelloWorldBean>(
+      `http://localhost:8080/hello-world/path-variable/${name}`,
+      {headers}
+    );
+    //console.log("Execute Hello World BEAN Service")
+  }
+  createBasicAuthenticationHttpHeader() {
+    let username = 'craig';
+    let password = 'puppy';
+    let basicAuthHeaderString =
+      'Basic ' + window.btoa(username + ':' + password);
+    return basicAuthHeaderString;
+  }
 }
+
+//Access to XMLHttpRequest at 'http://localhost:8080/hello-world/path-variable/craig'
+//from origin 'http://localhost:4200' has been blocked by CORS policy:
+//No 'Access-Control-Allow-Origin' header is present on the requested resource.
